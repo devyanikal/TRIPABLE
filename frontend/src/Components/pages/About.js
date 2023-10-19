e18d3380c302ab82e6c2049899095bce19f2a35e
Devyani Kalamkar <kalamkardevi12@gmail.com>
2023-10-19 18:19:23 +0530

    aboutus content done

--- frontend/src/Components/pages/About.js
+++ frontend/src/Components/pages/About.js
@@ -4,6 +4,7 @@ import './About.css'
 function About() {
   const about = require('../images/aboutus.jpeg');
   const founder= require('../images/founder.jpg');
+  const slogan= require('../images/slogan.jpg');
   return (
     <div>
       <div className='main-container'>
@@ -17,22 +18,20 @@ function About() {
         <div class="text">
         <h1>Founder</h1>
           <h2 class="text">
-            <p>Imagine as a child never been on a family holiday. Watching your class mates going for one in summer and winter vacations, and your travels limited to just school picnics or grandparents houses.</p>
-            <p>Hi, I am Neha Arora, and I was that child. My father is blind and mother is a wheelchair user. The thought of traveling to unknown places raises a lot of questions in the minds of a person with disability. Inaccessibility, lack of basic amenities and societal prejudice are some of the barriers they face.</p>
-            <p>When we grew up and started to travel, every attempt was a surprise. You travel 2000 miles only to realize that the place is not accessible & you can’t go in, or it does not offer the kind of pleasant experience you came for. Thanks to insensitivity and social stigmas, which persist in the community.</p></h2>
+            <p>At Trip-Able, we've harnessed the power of data and the spirit of innovation to craft a travel experience that's as limitless as your dreams. Our team of final-year BTech Data Science students from Ramdeobaba College of Engineering and Management welcomes you to a world where 'limitations' simply means 'new possibilities.'</p>
+            <p>With every click, swipe, and scroll, we're rewriting the story of travel, one unforgettable adventure at a time. 'Exploration knows no bounds,' is not just a tagline for us; it's a mantra we live by. Join us on a journey that transforms 'accessible travel' into 'unstoppable memories' and discover a world where the only limit is your imagination.</p></h2>
         </div>
       </div>
       <div class="container">
         <div class="text">
         <h1>How it started</h1>
           <h2 class="text">
-            <p>I realized that if we as a family are facing such issues, others might also be feeling the same. And they might not be as fortunate as us to be pushed by family and friends to come out of house and enjoy life to the fullest.</p>
-            <p>When I looked for solutions there was none. I started talking to more and more people with disability and their dreams, apprehensions, and experiences of travel, both for work and leisure. This process of research and surveys continued for two years, before I took the final plunge to leave my corporate career at Adobe and started Planet Abled.</p>
-            <p>A first of its kind unique travel sojourn of the planet where people of all disabilities and without disabilities travel together.</p>
+            <p>The problem we found was at hand is lack of comprehensive accessibility information and resources for individuals with disabilities in the tourism industry. In today's society, where inclusivity and diversity are increasingly valued, there is a growing need for accessible travel solutions. </p>
+            <p>The tourism industry often overlooks the specific needs of people with disabilities, limiting their opportunities to explore the world. The Trip-Able platform addresses this issue by offering essential information, promoting collaboration with the hospitality sector, and encouraging people with disabilities to travel comfortably. In doing so, it not only caters to an underserved market but also aligns with the broader societal goal of promoting inclusivity and equal opportunities for all.</p>
           </h2>
         </div>
         <div class="image">
-            <img id='img1' src="https://planetabled.com/wp-content/uploads/2017/02/S2-e1496236084191.jpg"/>
+            <img id='img1' src={slogan}/>
           </div>
       </div>
       <div class="container">
@@ -42,9 +41,9 @@ function About() {
         <div class="text">
         <h1>What we offer</h1>
           <h2 class="text">
-            <p>Planet-Abled provides accessible travel solutions and leisure excursions for people with different disabilities. There are over a billion people with different disabilities in the world, but the percentage of them travelling is miniscule, owning to lack of accessible travel options available.</p>
-            <p>Planet Abled identified this gap and came forward to give them the freedom to travel no matter what their disability is! Whether they want to experience a small facet of the city they are in or they want to travel across multiple cities of their interest, we have something unique, safe and enjoyable for them.</p>
-            <p>Planet Abled is the only organization in the world, where we mix people of all disabilities together into one group and travel together.</p></h2>
+            <p>Tripable stands out as a pioneering solution for accessible travel. Its uniqueness lies in its holistic approach to empower individuals with disabilities on their vacation journeys. Tripable provides detailed information on accessible accommodations, tourist attractions, and travel assistance services. </p>
+            <p>What truly sets it apart is its inclusive ecosystem, encouraging collaboration with the hospitality sector to ensure a seamless and enjoyable experience for all travelers. </p>
+            <p>By bridging the gap between travelers with special needs and the industry, Tripable uniquely promotes inclusive tourism, making it a game-changer in the realm of accessible travel solutions.</p></h2>
         </div>
       </div>
     </div>
