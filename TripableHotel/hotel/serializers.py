82791b94afdac5ec2dc6c64f0221b5318076a950
muskan-setiya <muskansetiya1706@gmail.com>
2023-12-04 13:52:54 +0530

    error correction in hotel api

--- TripableHotel/hotel/serializers.py
+++ TripableHotel/hotel/serializers.py
@@ -14,36 +14,6 @@ class add_hotel_serializer(serializers.Serializer):
     password = serializers.CharField(style={'input_type': 'password', 'placeholder': 'Password'})
     cpassword = serializers.CharField(style={'input_type': 'password', 'placeholder': 'Password'})
 
-    # username = serializers.CharField(min_length = 2)
-    landmark = serializers.CharField(max_length=50)
-    city = serializers.CharField(max_length=50)
-    state = serializers.CharField(max_length=50)
-    country =serializers.CharField(max_length=50)
-    pincode =serializers.IntegerField(default=0)
-
-    prefix = serializers.CharField()
-    mobile = serializers.CharField()
-
-    visual_impaired= serializers.BooleanField(default=False)
-    wheelchair_user= serializers.BooleanField(default=False)
-    hearing_impaired= serializers.BooleanField(default=False)
-    speech_impaired= serializers.BooleanField(default=False)
-
-    roomtype1= serializers.CharField(max_length=100, default= '')
-    roomtype2= serializers.CharField(max_length=100, default= '')
-    roomtype3= serializers.CharField(max_length=100, default= '')
-
-    pricetype1= serializers.IntegerField(default=0)
-    pricetype2= serializers.IntegerField(default=0)
-    pricetype3= serializers.IntegerField(default=0)
-
-    numOftype1 = serializers.IntegerField(default=0)
-    numOftype2 = serializers.IntegerField(default=0)
-    numOftype3 = serializers.IntegerField(default=0)
-
-    facilityoftype1= serializers.CharField(max_length=500, default= '')
-    facilityoftype2= serializers.CharField(max_length=500, default= '')
-    facilityoftype3= serializers.CharField(max_length=500, default= '')
 
 #extra
 class  getData_serializer(serializers.ModelSerializer):
